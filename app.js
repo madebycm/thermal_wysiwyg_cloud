6bf1333e85c0491a9b6510caa755c7a1c536b417
madebycm <christian.meinhold@gmail.com>
2014-01-29 22:02:41 +0100

    Initial hack

---
+++ app.js
@@ -0,0 +1,29 @@
+var express = require('express')
+  , app = express()
+  , port = 9911
+
+  , fs = require('fs')
+  , sys = require('sys')
+  , exec = require('child_process').exec
+  , child
+;
+
+app.use(express.static(__dirname + '/page'));
+app.use(express.bodyParser());
+
+app.get('/cap', function(req,res){
+  exec('phantomjs phantom.js', function(err, stdout){
+    fs.writeFile("page/status", "FRESH", function(){
+      res.end("Captured");
+    });
+  })
+})
+
+app.post('/save', function(req,res){
+  fs.writeFile('page/markup.html', req.body.markup, function(){
+    res.redirect('/');
+  })
+})
+
+console.log("OK @ " + port);
+app.listen(port);
